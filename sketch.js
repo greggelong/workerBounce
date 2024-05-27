ff36c99c80549c4351db356f189ad4cdd9910415
Greg K <gkreisman_cep@qq.com>
2024-05-27 17:23:01 +0800

    add the three of us

--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
 let gregs = [];
-let img 
-function preload(){
-    img = loadImage("greg.png")
+let img = [];
+function preload() {
+  img[0] = loadImage("yuan.png");
+  img[1] = loadImage("greg.png");
+  img[2] = loadImage("david.png");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  pixelDensity(1); 
-  imageMode(CENTER)
-  angleMode(DEGREES)
-  print(img)
-  img.resize(300,0)
-  for (let i =0; i<16; i++){
-  gregs[i] = new Ball(width/2,height/2);
+  pixelDensity(1);
+  imageMode(CENTER);
+  angleMode(DEGREES);
+  print(img);
+  img[1].resize(300, 0);
+  img[0].resize(0, 400);
+  img[2].resize(0, 400);
+  for (let i = 0; i < 26; i++) {
+    gregs[i] = new Ball(width / 2, height / 2);
   }
 }
 
 function draw() {
   background(255);
-  for (let i =0; i < gregs.length; i++){
-    
-  gregs[i].update();
-  gregs[i].show();
-  gregs[i].edges();
+  for (let i = 0; i < gregs.length; i++) {
+    gregs[i].update();
+    gregs[i].show();
+    gregs[i].edges();
   }
 }
